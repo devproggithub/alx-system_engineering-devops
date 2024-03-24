0452b667598ecb4acf515d6e89890cb2469d806e
devproggithub <dev.prognft@gmail.com>
2024-03-24 08:43:14 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
-# A puppet manifest installing flask v2.1.0 which is a package from pip3.
-# Am also specifying the version of flask to install using the ensure attribute
-package { 'flask':
+# Install flask
+package{'flask':
   ensure   => '2.1.0',
-  provider => pip3,
+  provider => 'pip3'
+}
+package{'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
