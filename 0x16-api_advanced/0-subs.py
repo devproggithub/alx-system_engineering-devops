10b1f0eb87a16bde4ae07b16ef0cff4377709232
devproggithub <dev.prognft@gmail.com>
2024-05-08 12:07:01 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-""" module of the function number_of_subscribers """
+"""
+Contains the number_of_subscribers function
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Retrieve the number of subscribers for a given subreddit using
-    the Reddit API.
-
-    Args:
-        subreddit (str): The name of the subreddit for which to
-        retrieve subscriber count.
-
-    Returns:
-        int: The number of subscribers for the specified subreddit,
-        or 0 if not found or an error occurs.
-    """
-    if subreddit is None:
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
-    user_agent = {"User-Agent": "ALX project about advanced api"}
-
-    response = requests.get(url, headers=user_agent).json()
-
-    return response.get("data", {}).get("subscribers", 0)
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
